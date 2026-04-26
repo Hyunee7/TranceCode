2b6ffd169bed45d6e6b237ae85f6de1e7ffaae24
Hyunee <107051977+Hyunee7@users.noreply.github.com>
2026-04-26 12:21:00 +0900

    최적화
    - 입력 소스를 2개에서 1개로 줄임.
    - 언어 설정은 동일 값이므로 설정을 삭제함.

--- AC3TrackAdd.bat
+++ AC3TrackAdd.bat
@@ -1,17 +1,16 @@
 \ffmpeg ^
  -i %1 ^
- -i %1 ^
  -map 0:0 ^
  -c:v copy ^
  -map 0:1 ^
- -metadata:s:a:0 language=chi -metadata:s:a:0 title="Stereo" ^
+ -metadata:s:a:0 title="Stereo" ^
  -disposition:a:0 default ^
  -c:a copy ^
  -map "[a]" ^
  -ar 48000 ^
- -metadata:s:a:1 language=chi -metadata:s:a:1 title="ac3 5.1ch Sound" ^
+ -metadata:s:a:1 title="ac3 5.1ch Sound" ^
  -disposition:a:1 0 ^
- -filter_complex "[1:a]pan=5.1(side)|FL=FL|FR=FR|FC=0.2*FR+0.2*FL|LFE<FL+FR|SL=FL-FR|SR=FR-FL[a]" ^
+ -filter_complex "[0:a]pan=5.1(side)|FL=FL|FR=FR|FC=0.2*FR+0.2*FL|LFE<FL+FR|SL=FL-FR|SR=FR-FL[a]" ^
  -c:a ac3 -b:a 320k ^
  -y ^
  "%~n1.AC3.Hyunee%~x1"
